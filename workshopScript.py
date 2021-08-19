40c1b47acaceec17ccd6898a2441107beadaaf94
Brett Parker <b.parker1@griffith.edu.au>
2021-08-19 14:53:51 +1000

    super confused, end of day git add .

--- workshopScript.py
+++ workshopScript.py
@@ -66,19 +66,80 @@ for i in range(0,len(primes),1):
     print("the value of i in this loop is:", primes[i])
 
 
+# if statements
+mass = 15.02
+
+if mass > 3.0 and mass <= 10.0:
+    print(mass, 'is large')
+elif mass > 10.0:
+    print(mass, 'reduclously is large')
+else:
+    print(mass, 'is not large')
+
+
+masses = [2.4, 7.4, 1.1, 52.3]
+
+for i in masses:
+    if i > 3.0 or i <= 10.0:
+        print(i, 'is large')
+    elif i > 10.0:
+        print(i, 'reduclously is large')
+    else:
+        print(i, 'is not large')
+
+
+masses = [2.4, 7.4, 1.1, 52.3]
+velocity = [53.6, 100.44, 7.2, 10000.5]
+
+
+if masses[1] > 3.0 and masses[2] <= 10.0 or velocity[0] = 54:
+    print('all good')
+
+velocity[0] = 54
 
 
 
 
+import glob
+import pandas as pd
+
+for filename in glob.glob('/Users/s5001793/Downloads/data 2/*.csv'):
+    contents = pd.read_csv(filename)
+    #print(filename, len(contents))
+
+contents.shape[0]
+
+len(contents)
+
+def print_greeting():
+    print("hello!!!")
 
 
+print_greeting()
+
+def print_date(year, month, day):
+    joined = str(year) + '-' + str(month) + '-' + str(day)
+    print(joined)
+    return(joined)
+
+Date = print_date(1999, 12, 29)
 
 
+def average(values):
+    if len(values) == 0:
+        print("You must provide some values")
+        return None
+    return sum(values) / len(values)
 
+def report(pressure):
+    #print('pressure is', pressure)
+    return(pressure)
 
+print('calling', report(22.5))
 
+report(22.5)
 
-
+r = report(22.5)
 
 
 
